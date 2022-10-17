eec5eed2eca5f757bbeada2bd8ed6c2a61a0a878
HenriquePavoni <114189078+HenriquePavoni@users.noreply.github.com>
2022-10-16 21:25:19 -0300

    Boletim criado em python
    É possível adicionar um numero indeterminado de alunos, e assim quando finalizado será mostrado o código, nome, média e se foi aprovado ou não

---
+++ boletim.py
@@ -0,0 +1,26 @@
+dados = dict()
+list1 = list()
+i = 0
+while True:
+    i = i + 1
+    dados['nome'] = str(input(f'\nDigite o nome do aluno {i}: '))
+    media = float(input(f'Digite a media do aluno {i}: '))
+    dados['media'] = media
+    if media >= 7:
+        dados['status'] = 'aprovado'
+    else:
+        dados['status'] = 'reprovado'
+
+    list1.append(dados.copy())
+
+    resp = str(input('\nDeseja continuar ? [S/N]: ')).upper().strip()
+    if resp == 'N':
+        break
+
+print('\nCod', end='    ')
+for n in dados.keys():
+    print(f'{n:<12}', end='')
+print()
+for count, m in enumerate(list1):
+    print(f'{count + 1:<7}{m["nome"]:<14}{m["media"]:<10}{m["status"]:}')
+print()
